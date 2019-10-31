1f7028e71127869a6b8a8bca44c6749a9615035b
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-31 13:37:55 -0400

    Updated
    Added sessions secret in the file with "administrator" also enabled sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "administrator"
+    set :session_secret, "administrators"
   end
 
   get "/" do
