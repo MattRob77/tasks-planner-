826e6a016130dbd3ce1b8a19b1fdd29c9d343b9f
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-31 13:45:13 -0400

    updated the user model
    Added validations and uniqueness to the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :tasks
   has_secure_password
 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: trues
   validates :email, uniqueness: true
   validates :name, presence: true
   validates :email, presence: true 
